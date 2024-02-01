ed4b1a48c112e43b0adc282c0d6e9f87bcd38470
Javi-Gallego <galgar@gmail.com>
2024-02-02 00:18:04 +0100

    Mejora encendido

--- js/main.js
+++ js/main.js
@@ -7,9 +7,22 @@ var chann2 = document.getElementById("canal2")
 var TVon = false;
 
 onoffButton.addEventListener("click", () => {
+    if (TVon === false) {
+        console.log("ahora me ves")
     TVon = true
     chann1.classList.add("visible")
+    chann1.classList.remove("oculto")
     chann1.play()
+    }
+    if (TVon === true) {
+        chann1.pause()
+        chann2.pause()
+        chann1.classList.remove("visible")
+        chann1.classList.add("oculto")
+        chann2.classList.add("oculto")
+        chann2.classList.remove("visible")
+        TVon = false
+    }
 })
 
 chann1Button.addEventListener("click", () => {
