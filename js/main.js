c5de015bb5c67182932f467310657b2b4348d450
Javi-Gallego <galgar@gmail.com>
2024-02-02 01:33:29 +0100

    Añadidos canales y nuevos controles

--- js/main.js
+++ js/main.js
@@ -1,48 +1,92 @@
+
+
 var onoffButton = document.getElementById("btnOnOff")
 var chann1Button = document.getElementById("channel1")
 var chann2Button = document.getElementById("channel2")
+var chann3Button = document.getElementById("channel3")
+var chann4Button = document.getElementById("channel4")
+var muteButton = document.getElementById("btnMute")
 var chann1 = document.getElementById("canal1")
 var chann2 = document.getElementById("canal2")
+var chann3 = document.getElementById("canal3")
+var chann4 = document.getElementById("canal4")
 
 var TVon = false;
 
 onoffButton.addEventListener("click", () => {
     if (TVon === false) {
-        console.log("ahora me ves")
-    TVon = true
+
     chann1.classList.add("visible")
-    chann1.classList.remove("oculto")
     chann1.play()
+    TVon = true
     }
     if (TVon === true) {
         chann1.pause()
         chann2.pause()
+        chann3.pause()
+        chann4.pause()
         chann1.classList.remove("visible")
         chann1.classList.add("oculto")
         chann2.classList.add("oculto")
         chann2.classList.remove("visible")
+        chann3.classList.add("oculto")
+        chann3.classList.remove("visible")
+        chann4.classList.add("oculto")
+        chann4.classList.remove("visible")
         TVon = false
     }
 })
 
 chann1Button.addEventListener("click", () => {
+    if(!chann2.paused) {
     chann2.pause()
-    chann1.play()
-    chann1.classList.add("visible")
-    chann1.classList.remove("oculto")
-    chann2.classList.add("oculto")
+    }
+    if(!chann3.paused) {
+        chann3.pause()
+    }
     chann2.classList.remove("visible")
-    console.log("canal1 " + chann1)
-    console.log("canal2 " + chann2)
+    chann2.classList.add("oculto")
+    chann3.classList.remove("visible")  
+    chann3.classList.add("oculto")
+    chann4.classList.remove("visible")
+    chann4.classList.add("oculto")
+    chann1.classList.remove("oculto")
+    chann1.classList.add("visible")
+    chann1.play()
 })
 
 chann2Button.addEventListener("click", () => {
     chann1.pause()
+    chann3.pause()
     chann2.play()
     chann2.classList.add("visible")
     chann2.classList.remove("oculto")
     chann1.classList.remove("visible")
     chann1.classList.add("oculto")
-    console.log("canal1 " + chann1)
-    console.log("canal2 " + chann2)
+    chann3.classList.add("oculto")
+    chann3.classList.remove("visible")
+    chann4.classList.add("oculto")
+    chann4.classList.remove("visible")
+})
+
+chann3Button.addEventListener("click", () => {
+    chann1.pause()
+    chann2.pause()
+    chann3.play()
+    chann3.classList.add("visible")
+    chann3.classList.remove("oculto")
+    chann1.classList.remove("visible")
+    chann1.classList.add("oculto")
+    chann2.classList.add("oculto")
+    chann2.classList.remove("visible")
+    chann4.classList.add("oculto")
+    chann4.classList.remove("visible")
+})
+
+muteButton.addEventListener("click", () => {
+    !chann1.muted ? chann1.muted = true : chann1.muted = false
+
+    chann2.muted = chann1.muted
+    chann3.muted = chann1.muted
+    chann4.muted = chann1.muted
 })
